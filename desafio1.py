04a94b76d9144b1196bc11cce8ad141df0b905bb
guilhermegrzelczak-lab <guilherme.grzelczak@escola.pr.gov.br>
2026-04-06 23:16:28 -0300

    desafio 1

---
+++ desafio1.py
@@ -0,0 +1,11 @@
+#soma dos numeros
+
+#pede os dois numeros ao usuario 
+num1 = int(input('diga o primeiro numero da soma: '))
+num2 = int(input('diga o segundo numero da soma: '))
+
+#cria uma variavel para guardar a soma dos numeros
+soma = num1 + num2
+
+#retorna a soma dos numeros ao usuario
+print(f"{num1} + {num2} é igual a: {soma}")
